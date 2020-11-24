15c6b87b72891482e906160994fce77bd08b3c03
18110282 <hieuho10022000@gmail.com>
2020-11-24 14:49:56 +0700

    update url of AdminHomeController

--- src/main/java/com/bookstore/controller/admin/AdminHomeController.java
+++ src/main/java/com/bookstore/controller/admin/AdminHomeController.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/admin/home")
+@WebServlet("/admin-home")
 public class AdminHomeController extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher rd = request.getRequestDispatcher("/views/admin/admin.jsp");
